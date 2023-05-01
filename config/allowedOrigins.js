2d308bf897f490554066e90eb3e67e2e74099392
Jordan Tyler <jmattyler@hotmail.com>
2023-04-30 21:21:25 -0500

    Cors update 5

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
     'https://dazzling-snickerdoodle-777101.netlify.app',
     'https://jordantylerinf653final.voltorik.repl.co',
-    'http://127.0.0.1:3000',
+    'http://127.0.0.1:5500',
     'http://localhost:3000'
 ];
 
